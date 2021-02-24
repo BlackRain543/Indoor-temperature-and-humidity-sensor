645404f2b56d36d70aca8e6f381210a02fef0d7e
BlackRain543 <64293995+BlackRain543@users.noreply.github.com>
2021-02-24 09:37:59 +0000

    Update ATmega328PB_OLED_SHT30.ino

--- ATmega328PB_OLED_SHT30/ATmega328PB_OLED_SHT30.ino
+++ ATmega328PB_OLED_SHT30/ATmega328PB_OLED_SHT30.ino
@@ -18,10 +18,9 @@
 //类实例化
 U8G2_SSD1306_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0,U8X8_PIN_NONE,19,18); //A5->19(SCK),A4->18(SDA),两页缓存
 ClosedCube_SHT31D sht30;
-SoftwareSerial TuyaSerial(0, 1); //MCU_RX, MCU_TX,与Tuya模块通信
 SoftwareSerial MySerial(12, 11); //RX, TX--调试
 
-float temp,humi;
+float temp = 11,humi = 20;    //温度，湿度
 
 //函数声明
 void SHT30ReadData(SHT31D result); 
@@ -35,7 +34,7 @@ void setup(void){
   u8g2.begin();                         //u8g2初始化  
   u8g2.enableUTF8Print();               //编码设置
   u8g2.setFont(u8g2_font_u8glib_4_tr);  //设置字体
-
+  
   //LED初始化
   pinMode(BluePin,  OUTPUT);            //Blue
   pinMode(GreenPin, OUTPUT);            //Green
@@ -43,13 +42,15 @@ void setup(void){
   pinMode(OrangePin,OUTPUT);            //Orange
   
   wifi_protocol_init();                 //Tuya-WiFi模块初始化
-  TuyaSerial.begin(9600);               //tuya串口初始化
-  MySerial.begin(9600);                 //调试串口初始化
+
+//  mcu_set_wifi_mode(SMART_CONFIG);
+  
+  Serial.begin(19200);                  //tuya串口初始化 （由于8MHz晶振，实际波特率9600）
+  MySerial.begin(19200);                //调试串口初始化 （由于8MHz晶振，实际波特率9600）
 }
 
 void loop(void)
 { 
-  wifi_uart_service();     //Wifi串口中断处理
   u8g2.firstPage();        // 标志图像循环的开始
   do{
     u8g2.setCursor(24,14);  u8g2.print("温湿度监测");
@@ -57,11 +58,14 @@ void loop(void)
     u8g2.setCursor(96,32);  u8g2.print("℃");
     u8g2.setCursor(8,48);   u8g2.print("湿度：");
     u8g2.setCursor(96,48);  u8g2.print("％");
-      
+
     SHT30ReadData(sht30.readTempAndHumidity(SHT3XD_REPEATABILITY_HIGH, SHT3XD_MODE_POLLING, 25));   //数据获取
   }while(u8g2.nextPage());  // 标志图像循环的结束
+  
+  wifi_uart_service();      //Wifi串口中断处理
+  Uart_Receive();
 
-  delay(5000);
+  delay(100);
 }
 
 void SHT30ReadData(SHT31D result) {
@@ -93,31 +97,31 @@ void LEDs(SHT31D result){
     }
     
     //温度状态指示
-    if(result.t>=30.0){
+    if(result.t>=32.0){
       digitalWrite(RedPin,1); digitalWrite(OrangePin,1);
       u8g2.setCursor(0,64);   u8g2.print("HOT!!!");
-    }else if(result.t>=25.0){
+    }else if(result.t>=27.0){
       digitalWrite(RedPin,1); digitalWrite(OrangePin,0);
       u8g2.setCursor(0,64);   u8g2.print("Hot!");
-    }else if(result.t<25.0){
+    }else if(result.t<27.0){
       digitalWrite(RedPin,0); digitalWrite(OrangePin,1);
     }
 
-    if(result.rh<60.0 && result.t<25.0){
+    if(result.rh<60.0 && result.t<27.0){
       u8g2.setCursor(48,64);  u8g2.print("Nice!");
     }
 }
 
 // 串口发送
 void Uart_PutChar(unsigned char value){
-  TuyaSerial.write(value);
+  Serial.write(value);
 }
 
 // 串口接收
 void Uart_Receive(){
-  if (TuyaSerial.available()) {
-    unsigned char value = TuyaSerial.read();
-    uart_receive_input(value);
-    MySerial.print(value);        //调试
+  if(Serial.available()){
+      unsigned char  value = (unsigned char)Serial.read();
+      uart_receive_input(value);
+//      MySerial.write(value); 
   }
 }
